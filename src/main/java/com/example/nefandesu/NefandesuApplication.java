948b9627f2d55e7c32905c81644096b25235948b
Micoko <112613372+rakaso598@users.noreply.github.com>
2024-08-09 22:16:03 +0900

    240809-init

---
+++ src/main/java/com/example/nefandesu/NefandesuApplication.java
@@ -0,0 +1,13 @@
+package com.example.nefandesu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NefandesuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NefandesuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nefandesu/NefandesuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nefandesu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NefandesuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
